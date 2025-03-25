051f39fc5afce4a13312964d5d133971e299fc71
Daniel Cardeal <danicardeal.08@gmail.com>
2025-03-25 10:19:33 -0300

    second commit

---
+++ api/banco.py
@@ -0,0 +1,42 @@
+import os
+import json
+import base64
+import gspread
+from google.oauth2.service_account import Credentials
+
+# Recuperar a variável de ambiente
+key_json_data = os.getenv("GOOGLE_KEY_JSON")
+
+# Decodificar a chave base64
+key_info = json.loads(base64.b64decode(key_json_data))
+
+# Criar as credenciais com a chave decodificada
+creds = Credentials.from_service_account_info(key_info)
+
+# Usar gspread com as credenciais
+gc = gspread.authorize(creds)
+
+CODE = "1B7huX51Ta8nyxYw-Jh04GUSzlhJTezBP6XcpHvErwx4"
+sheet = gc.open_by_key(CODE)
+caminho = "Obrigatórias"
+caminho_eletivas = "Eletivas"
+caminho_optativas = "Optativas"
+excel_obrigatorias = sheet.worksheet(caminho)
+excel_eletivas = sheet.worksheet(caminho_eletivas)
+excel_optativas = sheet.worksheet(caminho_optativas)
+
+
+len_semestre1 = len(excel_obrigatorias.col_values(2))
+len_semestre2 = len(excel_obrigatorias.col_values(7))
+len_semestre3 = len(excel_obrigatorias.col_values(12))
+len_semestre4 = len(excel_obrigatorias.col_values(17))
+len_semestre5 = len(excel_obrigatorias.col_values(22))
+len_semestre6 = len(excel_obrigatorias.col_values(27))
+len_semestre7 = len(excel_obrigatorias.col_values(32))
+len_semestre8 = len(excel_obrigatorias.col_values(37))
+
+
+len_semestre4_eletivas = len(excel_eletivas.col_values(2))
+len_semestre5_eletivas = len(excel_eletivas.col_values(7))
+
+len_optativas = len(excel_optativas.col_values(2))
---
+++ api/main.py
@@ -0,0 +1,46 @@
+import os
+from fastapi import FastAPI
+from api import banco
+
+app = FastAPI()
+
+
+
+@app.get('/')
+def landingPage():
+    return {"Mensagem:":"Teste Levi"}
+
+@app.get("/api/saudacao")
+def saudacao():
+    return {"message": "Olá, mundo!"}
+
+@app.get('/obrigatorias')
+def obrigatorias():
+    obrig = []
+    s1 = banco.excel_obrigatorias.get(f'B3:E{banco.len_semestre1}')
+    s2 = banco.excel_obrigatorias.get(f'G3:J{banco.len_semestre2}')
+    s3 = banco.excel_obrigatorias.get(f'L3:O{banco.len_semestre3}')
+    s4 = banco.excel_obrigatorias.get(f'Q3:T{banco.len_semestre4}')
+    s5 = banco.excel_obrigatorias.get(f'V3:Y{banco.len_semestre6}')
+    s6 = banco.excel_obrigatorias.get(f'AA3:AD{banco.len_semestre6}')
+    s7 = banco.excel_obrigatorias.get(f'AF3:AI{banco.len_semestre7}')
+    s8 = banco.excel_obrigatorias.get(f'AK3:AN{banco.len_semestre8}')
+    obrig.extend((s1, s2, s3, s4, s5, s6, s7, s8))
+    return obrig
+
+@app.get('/eletivas')
+def eletivas():
+    elet = []
+    s_e_4 = banco.excel_eletivas.get(f'B3:E{banco.len_semestre4_eletivas}')
+    s_e_5 = banco.excel_eletivas.get(f'G3:J{banco.len_semestre5_eletivas}')
+    elet.extend((s_e_4, s_e_5))
+    return elet
+
+@app.get('/optativas')
+def optativas():
+    optativa = banco.excel_optativas.get(f'B2:F{banco.len_optativas}')
+    return optativa
+
+@app.get('/final')
+def final():
+    return ('Acho que deu bom')
